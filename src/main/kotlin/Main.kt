203ed24217286f99cb508cfadf7d3964561bcc3b
Yaroslav Dosov <yaroslav.dosov@lightspeedhq.com>
2026-02-17 17:57:25 +0400

    fixed main()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,31 +9,5 @@ class BotApplication
 
 fun main() {
     runApplication<BotApplication>()
-
-    /*
-    val bot = bot {
-
-        token = System.getenv("BOT_TOKEN")
-        println(token)
-
-
-
-        dispatch {
-            text {
-                bot.sendMessage(
-                    chatId = ChatId.fromId(message.chat.id),
-                    messageThreadId = message.messageThreadId,
-                    text = text + " test 22",
-                    protectContent = true,
-                    disableNotification = false,
-                )
-            }
-        }
-    }
-
-    bot.startPolling()
-
-
-     */
 }
 
